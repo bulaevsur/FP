0a62d6f195ed5b9843f2be1bef07b72fbe57d95d
Sergey <bulaevsur@mail.ru>
2019-07-02 10:23:43 +0300

    ThirdLabChanges

---
+++ ConsoleApp3/ConsoleApp3/WWDB.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace MyProgram
+{
+    class WWDB
+    {
+        private SqlConnection conn;
+        /// <summary>
+        /// Конструктор, проверяющий, установлено ли соединение
+        /// </summary>
+        public WWDB()
+        {
+            if (conn == null || conn.State == System.Data.ConnectionState.Closed)
+            {
+                Connect();
+            }
+            else
+            {
+                Console.WriteLine("Already connected");
+            }
+        }
+        /// <summary>
+        /// Метод, устанавливающий соединение с БД
+        /// </summary>
+        public void Connect()
+        {
+            conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=
+            C:\Users\Unknown\Documents\Visual Studio 2017\Projects\ConsoleApp3\ConsoleApp3\DBmain.mdf;Integrated Security=True");
+            conn.Open();
+        }
+        /// <summary>
+        /// Метод, прерывающий соединение с БД
+        /// </summary>
+        public void StopConnect()
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+        /// <summary>
+        /// Метод, формирующий список объектов методом считывания данных из базы данных
+        /// </summary>
+        /// <returns></returns>
+        public List<animal> GetList()
+        {
+            var anims = new List<animal>();
+            using (SqlCommand command = new SqlCommand(@"SELECT Id, type, color, weight, predator FROM [DB_animals]", conn))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    animal a = null;
+                    while (reader.Read())
+                    {
+                        if (reader["type"].ToString() == "Wolf")
+                        {
+                            a = new wolf();
+                        }
+                        if (reader["type"].ToString() == "Dog")
+                        {
+                            a = new dog();
+                        }
+                        a.Serialize(reader);
+                        anims.Add(a);
+                    }
+                }
+            }
+            return anims;
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/dog.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SqlClient;
+
+namespace MyProgram
+{
+    class dog:mammal
+    {
+        public dog() : base() { }
+        public dog(string c, string t, int w, string p) : base(c, t, w, p) { }
+        public string dog_color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public string dog_type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public int dog_weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        public string dog_predator
+        {
+            get { return predator; }
+            set { predator = value; }
+        }
+        public override string info()
+        {
+            return base.info();
+        }
+        public override void Serialize(SqlDataReader reader)
+        {
+            base.Serialize(reader);
+        }
+    }
+}
